d16e131262212a5d15dff9a37aaf7fdb8358e05c
ellevalentine <elle.valentine@hotmail.com>
2019-06-21 17:55:45 +0100

    multi page modal working

--- Stocks-Frontend/js/MyJs/user.js
+++ Stocks-Frontend/js/MyJs/user.js
@@ -330,26 +330,26 @@ function pressDeleteUSerButton(user){
 
 {/* <script> */}
 
-  //pie
+//   //pie
 
-  const script = document.createElement("script")
+//   const script = document.createElement("script")
 
-  var ctxP = document.getElementById("pieChart").getContext('2d');
-  var myPieChart = new Chart(ctxP, {
-    type: 'pie',
-    data: {
-      labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-      datasets: [{
-        data: [300, 50, 100, 40, 120],
-        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-        hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-      }]
-    },
-    options: {
-      responsive: true
-    }
-  })
+//   var ctxP = document.getElementById("pieChart").getContext('2d');
+//   var myPieChart = new Chart(ctxP, {
+//     type: 'pie',
+//     data: {
+//       labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+//       datasets: [{
+//         data: [300, 50, 100, 40, 120],
+//         backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+//         hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+//       }]
+//     },
+//     options: {
+//       responsive: true
+//     }
+//   })
   
-  script.append(myPieChart)
+//   script.append(myPieChart)
 
-// </script>
+// // </script>
